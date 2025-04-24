a9ca5640323bd87e2edd1cdfc45f4d60cf20c916
realdegrees <18383355+realdegrees@users.noreply.github.com>
2025-04-25 01:02:39 +0200

    add default command
    This command provides --config and --verbose flags
    It evaluates the config recursively and send the evaluated payload to the dippit server defined in the config itself, this is done on an interval that is also based on a config value

--- dippid_sender/DIPPID_sender.py
+++ dippid_sender/DIPPID_sender.py
@@ -1,18 +1,53 @@
+from __future__ import annotations
+from typing import Callable, Dict, List, Union
+import click
 import socket
+import json
 import time
 
-IP = '127.0.0.1'
-PORT = 5700
+JSONType = Union[
+    Dict[str, "JSONType"],
+    List["JSONType"],
+    float,
+    str,
+    bool,
+    None
+]
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-counter = 0
+@click.command()
+@click.option('--config', '-c', required=True, help='JSON string or @path/to/file.json')
+@click.option('--verbose', '-v', required=True, is_flag=True, help='Enable to print messages')
+def run(config: str, verbose: bool):
+    cfg: JSONType = {}
+    try:
+        cfg = json.loads(config)
+    except json.JSONDecodeError:
+        try:
+            with open(config) as f:
+                cfg = json.load(f)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File not found: {config}")
 
-while True:
-    message = '{"heartbeat" : ' + str(counter) + '}'
-    print(message)
+    ip, port, interval = cfg.get('ip', '127.0.0.1'), cfg.get(
+        'port', 5700), cfg.get('interval', 100)
+    if verbose:
+        print(f"Sending to {ip}:{port} every {interval}ms")
 
-    sock.sendto(message.encode(), (IP, PORT))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    start_time = time.time()
 
-    counter += 1
-    time.sleep(1)
+    while True:
+        t = (time.time() - start_time)
+        payload = eval_cfg(cfg["mocks"], t)
+
+        message = json.dumps(payload)
+        if verbose:
+            print('→', message)
+        sock.sendto(message.encode(), (ip, port))
+
+        time.sleep(interval / 1000)
+
+
+if __name__ == "__main__":
+    run()
