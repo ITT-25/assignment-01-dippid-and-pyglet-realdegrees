bd33fdc9b6172f5375f2ddad8d71aca13d1cdb27
realdegrees <18383355+realdegrees@users.noreply.github.com>
2025-04-25 18:22:45 +0200

    add button support

--- dippid_sender/DIPPID_sender.py
+++ dippid_sender/DIPPID_sender.py
@@ -1,13 +1,41 @@
 from __future__ import annotations
+from collections import deque
 import math
 import click
 import socket
 import json
 import time
 from simpleeval import simple_eval
-from typing import TypedDict
+from typing import Optional, TypedDict
 from typing import Dict
 import random
+
+
+class ButtonState:
+    def __init__(self, name: str, expr: str):
+        self.name = name
+        self.expr = expr
+        self.state = False
+        self.value_history = deque(maxlen=3)
+
+    def set_value(self, value: float):
+        self.value_history.appendleft(value)
+
+    def get_value(self) -> Optional[float]:
+        if len(self.value_history) == 0:
+            return None
+        else:
+            return self.value_history[0]
+
+    def check_value_flip(self) -> bool:
+        """Check if the value has bounced at the upper limit"""
+        if len(self.value_history) < 3:
+            return False
+        else:
+            return self.value_history[0] < self.value_history[1] and self.value_history[1] > self.value_history[2]
+
+
+button_states: list[ButtonState] = list()
 
 
 class Config(TypedDict):
@@ -21,22 +49,44 @@ class EvaluatedConfig(Config):
     mocks: Dict[str, Dict[str, float]]
 
 
-def get_data(mocks: Dict[str, Dict[str, str]], t: float) -> Dict[str, Dict[str, float]]:
-    evaluated: Dict[str, Dict[str, float]] = {}
+def get_data(mocks: Dict[str, Dict[str, str]], t: float) -> Dict[str, Dict[str, float | ButtonState]]:
+    evaluated: Dict[str, Dict[str, float | ButtonState]] = {}
 
-    for capability, values in mocks.items():
+    def evaluate_leaf(name: str, leaf: str) -> float:
+        # Default math evaluation
+        if not leaf.startswith('button:'):
+            evaluated[capability][name] = evaluate_math_expr(leaf, t)
+            return
+
+        # Special case for buttons
+        button = next((b for b in button_states if b.name == name), None)
+        if not button:
+            button = ButtonState(name, leaf.split(':', 1)[1])
+            button_states.append(button)
+
+        button.set_value(evaluate_math_expr(button.expr, t))
+        evaluated[capability][name] = 1 if button.check_value_flip() else 0
+
+    for capability, value in mocks.items():
         evaluated[capability] = {}
-        for value_name, leaf in values.items():
-            if not isinstance(leaf, str):
-                raise TypeError(
-                    f"Invalid configuration for {capability}: {leaf}")
-            else:
-                evaluated[capability][value_name] = evaluate(leaf, t)
+
+        if isinstance(value, str):
+            evaluate_leaf(capability, value)
+            continue
+        elif isinstance(value, dict):
+            for value_name, leaf in value.items():
+                if not isinstance(leaf, str):
+                    raise TypeError(
+                        f"Invalid configuration for {capability}.{value_name}: Expected str, got {type(leaf)}")
+                evaluate_leaf(value_name, leaf)
+        else:
+            raise TypeError(
+                f"Invalid configuration for {capability}: Expected str or dict, got {type(value)}")
 
     return evaluated
 
 
-def evaluate(math_expr: str, t: float) -> float:
+def evaluate_math_expr(math_expr: str, t: float) -> float:
     names = {'t': t}
     functions = {
         'sin': lambda n: 0.5*(1 + math.sin(2 * n * math.pi)), 'cos': lambda n: 0.5*(1 + math.cos(2 * n * math.pi)), 'tan': lambda n: 0.5*(1 + math.tan(2 * n * math.pi)),
@@ -71,7 +121,6 @@ def run(config: str, verbose: bool):
     while True:
         t = (time.time() - start_time)
         payload = get_data(cfg["mocks"], t)
-
         message = json.dumps(payload)
         if verbose:
             print('→', message)
