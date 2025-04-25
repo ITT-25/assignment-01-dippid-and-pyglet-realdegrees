5a3474c080f3f3f05723f78bd2a34e33db67c747
realdegrees <18383355+realdegrees@users.noreply.github.com>
2025-04-25 20:58:05 +0200

    reduce complexity

--- dippid_sender/DIPPID_sender.py
+++ dippid_sender/DIPPID_sender.py
@@ -13,39 +13,24 @@ import random
 
 DEFAULT_PORT = 5700
 DEFAULT_IP = '127.0.0.1'
-DEFAULT_INTERVAL = 2
+DEFAULT_INTERVAL = 50
 
 
 class ButtonState:
-    """Class to persist buttons across multiple loops.
-
-    Stores the last 3 values to determine if the curve has flipped direction.
-    """
+    """Helper class to persist buttons across multiple loops. Stores the last 3 values to determine if the curve has flipped direction."""
 
     def __init__(self, name: str, expr: str):
         self.name = name
         self.expr = expr
-        self.state = False
-        self.value_history = deque(maxlen=3)
+        self.values = deque(maxlen=3)
 
-    def set_value(self, value: float):
-        self.value_history.appendleft(value)
-
-    def get_value(self) -> Optional[float]:
-        if len(self.value_history) == 0:
-            return None
-        else:
-            return self.value_history[0]
-
-    def is_pressed(self) -> bool:
-        """Check if the value has bounced at the upper limit"""
-        if len(self.value_history) < 3:
+    def update(self, t: float) -> bool:
+        val = evaluate_expr(self.expr, t)
+        self.values.appendleft(val)
+        if len(self.values) < 3:
             return False
-        else:
-            return self.value_history[0] < self.value_history[1] and self.value_history[1] > self.value_history[2]
-
-
-button_states: list[ButtonState] = list()
+        # Detect a local maximum
+        return self.values[0] < self.values[1] > self.values[2]
 
 
 class Config(TypedDict):
@@ -59,67 +44,30 @@ MockConfig = Dict[str, Dict[str, str] | str]
 MockData = Dict[str, Dict[str, float] | float]
 
 
-def get_data(mocks: MockConfig, t: float) -> MockData:
-    evaluated: MockData = {}
-
-    def evaluate(capability: str, name: Optional[str], value: str) -> float:
-        """Evaluate the math expression and store the result in evaluated."""
-        # Default math evaluation
-        if not value.startswith('button:'):
-            if name is None:
-                evaluated[capability] = evaluate_math_expr(value, t)
-            else:
-                evaluated[capability][name] = evaluate_math_expr(value, t)
-            return
-
-        # Special case for buttons
-        button = next((b for b in button_states if b.name == name), None)
-        if not button:
-            button = ButtonState(name, value.split(':', 1)[1])
-            button_states.append(button)
-
-        button.set_value(evaluate_math_expr(button.expr, t))
-        if name is None:
-            evaluated[capability] = 1 if button.is_pressed() else 0
-        else:
-            evaluated[capability][name] = 1 if button.is_pressed() else 0
-
-    for capability, value in mocks.items():
-        evaluated[capability] = {}
-
-        if isinstance(value, str):
-            evaluate(capability, None, value)
-        elif isinstance(value, dict):
-            for key, value in value.items():
-                if not isinstance(value, str):
-                    raise TypeError(
-                        f"Invalid configuration for {capability}.{key}: Expected str, got {type(value)}")
-                evaluate(capability, key, value)
-        else:
-            raise TypeError(
-                f"Invalid configuration for {capability}: Expected str or dict, got {type(value)}")
-
-    return evaluated
-
-
-def evaluate_math_expr(math_expr: str, t: float) -> float:
-    names = {'t': t}
+def evaluate_expr(math_expr: str, t: float) -> float:
     functions = {
-        'sin': lambda n: 0.5*(1 + math.sin(2 * n * math.pi)), 'cos': lambda n: 0.5*(1 + math.cos(2 * n * math.pi)), 'tan': lambda n: 0.5*(1 + math.tan(2 * n * math.pi)),
-        'sqrt': math.sqrt, 'log': math.log, 'exp': math.exp,
-        'abs': abs, 'pow': math.pow, 'random': lambda: random.uniform(0, 1)
+        'sin': lambda n: 0.5*(1+math.sin(2*math.pi*n)),
+        'cos': lambda n: 0.5*(1+math.cos(2*math.pi*n)),
+        'tan': lambda n: 0.5*(1+math.tan(2*math.pi*n)),
+        'sqrt': math.sqrt,
+        'log': math.log,
+        'exp': math.exp,
+        'abs': abs,
+        'pow': math.pow,
+        'random': lambda: random.random()
     }
     try:
-        return simple_eval(math_expr, functions=functions, names=names)
+        return simple_eval(math_expr, functions=functions, names={'t': t})
     except Exception:
         print(f"Unable to evaluate '{math_expr}'")
         return 0.0
 
 
 @click.command()
-@click.option('--config', '-c', required=True, help='JSON string or @path/to/file.json')
+@click.option('--config', '-c', required=True, help='JSON string or path/to/file.json', type=str)
 @click.option('--verbose', '-v', required=False, is_flag=True, help='Enable to print messages')
-def run(config: str, verbose: bool):
+@click.option('--truncate', '-t', required=False, help='Truncate values to this many decimal places', type=int)
+def run(config: str, verbose: bool, truncate: Optional[int]):
     cfg: Config = {}
     try:
         cfg = json.loads(config)
@@ -130,29 +78,52 @@ def run(config: str, verbose: bool):
         except FileNotFoundError:
             raise FileNotFoundError(f"File not found: {config}")
 
-    ip, port, interval, mocks = (
-        cfg.get('ip', DEFAULT_IP),
-        cfg.get('port', DEFAULT_PORT),
-        cfg.get('interval', DEFAULT_INTERVAL),
-        cfg.get('mocks', {})
-    )
-    
+    ip = cfg.get('ip', DEFAULT_IP)
+    port = cfg.get('port', DEFAULT_PORT)
+    interval = cfg.get('interval', DEFAULT_INTERVAL)
+    mocks = cfg.get('mocks', {})
+
     if verbose:
         print(
             f"Sending to {ip}:{port} every {interval}ms\nConfig:\n{json.dumps(mocks, indent=2)}")
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    start_time = time.time()
+    start = time.time()
+    buttons: Dict[str, ButtonState] = {}
 
     while True:
-        t = (time.time() - start_time)
-        payload = get_data(cfg["mocks"], t)
-        message = json.dumps(payload)
+        t = time.time() - start
+        data: MockData = {}
+
+        for capability, value in mocks.items():
+            if isinstance(value, str):
+                value = {None: value}
+
+            sub: Dict[str, float] = {}
+            for key, expr in value.items():
+                if expr.startswith('button:'):
+                    button_name = (capability + '.' + key) if key else capability
+                    button = buttons.get(button_name, None) or ButtonState(
+                        button_name, expr.split(':', 1)[1])
+                    
+                    if button_name not in buttons:
+                        buttons[button_name] = button
+
+                    button_pressed = button.update(t)
+                    result = 1 if button_pressed else 0
+                else:
+                    result = evaluate_expr(expr, t)
+
+                if truncate is not None and truncate >= 0:
+                    result = round(result, truncate)
+                sub[key] = result
+
+            data[capability] = sub.get(None) if None in sub else sub
+
+        msg = json.dumps(data)
         if verbose:
-            print('→', message)
-
-        sock.sendto(message.encode(), (ip, port))
-
+            print('→', msg)
+        sock.sendto(msg.encode(), (ip, port))
         time.sleep(interval / 1000)
 
 
