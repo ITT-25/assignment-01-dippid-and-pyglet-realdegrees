094a25fe38b787f93ea2b1d21c34a1caf54ec3b7
realdegrees <18383355+realdegrees@users.noreply.github.com>
2025-04-27 13:30:11 +0200

    remove remnants of launch post reset logic

--- 2d_game/src/managers/game_manager.py
+++ 2d_game/src/managers/game_manager.py
@@ -13,11 +13,13 @@ from src.util import GameState, Vector2D
 if TYPE_CHECKING:
     from game import GameWindow
 
+
 class GameManager():
     """GameManager is responsible for managing the game state and updating game objects."""
+
     def __init__(self, window: "GameWindow"):
         self.window = window
-        
+
         # Init Ball
         self.ball = Ball(self.window.width / 2,
                          self.window.height / 2, 12, window)
@@ -48,7 +50,6 @@ class GameManager():
 
         self.state = GameState.INACTIVE
         self.reset_timer = 0
-        self.launch_timer = 0
         self.last_scorer = None
 
     def reset(self):
@@ -56,8 +57,7 @@ class GameManager():
         self.ball.visible = True
         self.state = GameState.WAITING
         self.reset_timer = 0
-        self.launch_timer = 0
-        
+
     def update(self, delta_time: float):
         was_ball_visible = self.ball.visible
 
@@ -65,12 +65,12 @@ class GameManager():
         self.ball.update(delta_time)
         self.paddle_left.update(delta_time)
         self.paddle_right.update(delta_time)
-        
+
         # Restricts npc movement during non-playing states
         if self.state == GameState.PLAYING:
             self.player_1.update(delta_time)
             self.player_2.update(delta_time)
-            
+
         self.collision_manager.update(delta_time)
 
         # State transitions
@@ -81,8 +81,9 @@ class GameManager():
         elif self.state == GameState.WAITING:
             # Wait for button_1 from either player
             if self.player_1.is_ready() and self.player_2.is_ready():
-                self.ball.set_velocity((self.paddle_left.get_center(
-                ) - self.ball.get_center()).normalize() * INITIAL_BALL_SPEED)
+                self.ball.set_velocity((
+                    self.paddle_left.get_center() - self.ball.get_center()
+                ).normalize() * INITIAL_BALL_SPEED)
                 self.state = GameState.PLAYING
 
         elif self.state == GameState.PLAYING:
@@ -95,21 +96,14 @@ class GameManager():
                     self.last_scorer = self.player_1
                 self.state = GameState.RESETTING
                 self.reset_timer = 1.0
-                self.launch_timer = 1.0
 
         elif self.state == GameState.RESETTING:
             self.reset_timer -= delta_time
             self.paddle_left.set_velocity(Vector2D(0, 0))
             self.paddle_right.set_velocity(Vector2D(0, 0))
-            if self.reset_timer <= 0 and self.launch_timer > 0:
+            if self.reset_timer <= 0:
                 self.reset()
-                self.launch_timer -= delta_time
-            elif self.launch_timer <= 0:
-                self.ball.set_velocity((self.last_scorer.paddle.get_center(
-                ) - self.ball.get_center()).normalize() * INITIAL_BALL_SPEED)
-                self.state = GameState.PLAYING
 
-    
     def exit(self):
         # Disconnect DIPPID sensors to stop them from keeping the process alive on exit
         self.player_1.disconnect()
